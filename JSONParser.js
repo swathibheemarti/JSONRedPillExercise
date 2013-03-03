d01beddc392f3fd20a8f34a3d2eda3d024d19f0d
swathi@bheemarthis.com <swathi@bheemarthis.com>
2013-03-03 15:45:15 -0500

    Adding getWeekDayHours, testing with JSON...Swathi

--- JSONParser.js
+++ JSONParser.js
@@ -1,10 +1,15 @@
 var http = require('http');
 var moment = require('./moment');
 
-http.get("http://maccherone.com/share/1000-snapshots-overlap-with-Feb-2012.json", function(res){
+http.get("http://localhost/test.json", function(res){
 
     var jsonString = '';
-    var feb2012JobDaysCnt = 0;
+
+    var feb2012IdeaHoursCnt = 0;
+    var feb2012DefinedHoursCnt = 0;
+    var feb2012InProgressHoursCnt = 0;
+    var feb2012CompletedHoursCnt = 0;
+    var feb2012AcceptedHoursCnt = 0;
 
     //console.log("Got response: " + res.statusCode);
 
@@ -19,30 +24,89 @@ http.get("http://maccherone.com/share/1000-snapshots-overlap-with-Feb-2012.json"
         var json = JSON.parse(jsonString);
         var validFrom;
         var validTo;
-        var febStart = moment('2012-02-01T00:00:00.000Z', "YYYY-MM-DDTHH:mm:ss.SSSZ");
-        var febEnd = moment('2012-03-01T00:00:00.000Z', "YYYY-MM-DDTHH:mm:ss.SSSZ");
 
         for(var i = 0; i < json.length; i++){
 
-            validFrom = moment(jason[i]._ValidFrom, "YYYY-MM-DDTHH:mm:ss.SSSZ");
-            validTo = moment(jason[i]._ValidTo, "YYYY-MM-DDTHH:mm:ss.SSSZ");
+            var febStart = moment('2012-02-01T00:00:00.000Z', "YYYY-MM-DDTHH:mm:ss.SSSZ");
+            var febEnd = moment('2012-03-01T00:00:00.000Z', "YYYY-MM-DDTHH:mm:ss.SSSZ");
 
-            if((validFrom.isBefore(febStart) || validFrom.isSame(febStart)) && validTo.isAfter(febStart)){
+            validFrom = moment(json[i]._ValidFrom, "YYYY-MM-DDTHH:mm:ss.SSSZ");
+            validTo = moment(json[i]._ValidTo, "YYYY-MM-DDTHH:mm:ss.SSSZ");
+
+            if(validTo.year() >= '2999'){
+                validTo = moment('2012-03-01T00:00:00.000Z', "YYYY-MM-DDTHH:mm:ss.SSSZ");
+            }
+
+            if(validFrom.isBefore(febStart) && validTo.isAfter(febStart)){
                 if(validTo.isBefore(febEnd)){
-                    feb2012JobDaysCnt += getWeekDays(febStart, validTo);
-                }else if(validTo.isAfter(febEnd) || validTo.isSame(febEnd)){
-                    feb2012JobDaysCnt += getWeekDays(febStart, febEnd);
+
+                    if(json[i].ScheduleState == "In-Progress"){
+                        feb2012InProgressHoursCnt += getWeekDayHours(febStart, validTo, validFrom, validTo);
+                    }else if(json[i].ScheduleState == "Completed"){
+                        feb2012CompletedHoursCnt += getWeekDayHours(febStart, validTo, validFrom, validTo);
+                    }else if(json[i].ScheduleState == "Accepted"){
+                        feb2012AcceptedHoursCnt += getWeekDayHours(febStart, validTo, validFrom, validTo);
+                    }else if(json[i].ScheduleState == "Idea"){
+                        feb2012IdeaHoursCnt += getWeekDayHours(febStart, validTo, validFrom, validTo);
+                    }else if(json[i].ScheduleState == "Defined"){
+                        feb2012DefinedHoursCnt += getWeekDayHours(febStart, validTo, validFrom, validTo);
+                    }
+
+                }else if(validTo.isAfter(febEnd)){
+
+                    if(json[i].ScheduleState == "In-Progress"){
+                        feb2012InProgressHoursCnt += getWeekDayHours(febStart, validTo, validFrom, validTo);
+                    }else if(json[i].ScheduleState == "Completed"){
+                        feb2012CompletedHoursCnt += getWeekDayHours(febStart, validTo, validFrom, validTo);
+                    }else if(json[i].ScheduleState == "Accepted"){
+                        feb2012AcceptedHoursCnt += getWeekDayHours(febStart, validTo, validFrom, validTo);
+                    }else if(json[i].ScheduleState == "Idea"){
+                        feb2012IdeaHoursCnt += getWeekDayHours(febStart, validTo, validFrom, validTo);
+                    }else if(json[i].ScheduleState == "Defined"){
+                        feb2012DefinedHoursCnt += getWeekDayHours(febStart, validTo, validFrom, validTo);
+                    }
+
                 }
-            }else if(validFrom.isAfter(febStart) && (validFrom.isBefore(febEnd) || validFrom.isSame(febEnd)) && validTo.isAfter(validFrom)){
+            }else if(validFrom.isAfter(febStart) && validFrom.isBefore(febEnd)){
                 if(validTo.isBefore(febEnd)){
-                    feb2012JobDaysCnt += getWeekDays(validFrom, validTo);
+
+                    if(json[i].ScheduleState == "In-Progress"){
+                        feb2012InProgressHoursCnt += getWeekDayHours(febStart, validTo, validFrom, validTo);
+                    }else if(json[i].ScheduleState == "Completed"){
+                        feb2012CompletedHoursCnt += getWeekDayHours(febStart, validTo, validFrom, validTo);
+                    }else if(json[i].ScheduleState == "Accepted"){
+                        feb2012AcceptedHoursCnt += getWeekDayHours(febStart, validTo, validFrom, validTo);
+                    }else if(json[i].ScheduleState == "Idea"){
+                        feb2012IdeaHoursCnt += getWeekDayHours(febStart, validTo, validFrom, validTo);
+                    }else if(json[i].ScheduleState == "Defined"){
+                        feb2012DefinedHoursCnt += getWeekDayHours(febStart, validTo, validFrom, validTo);
+                    }
+
                 }else{
-                    feb2012JobDaysCnt += getWeekDays(validFrom, febEnd);
+
+                    if(json[i].ScheduleState == "In-Progress"){
+                        feb2012InProgressHoursCnt += getWeekDayHours(febStart, validTo, validFrom, validTo);
+                    }else if(json[i].ScheduleState == "Completed"){
+                        feb2012CompletedHoursCnt += getWeekDayHours(febStart, validTo, validFrom, validTo);
+                    }else if(json[i].ScheduleState == "Accepted"){
+                        feb2012AcceptedHoursCnt += getWeekDayHours(febStart, validTo, validFrom, validTo);
+                    }else if(json[i].ScheduleState == "Idea"){
+                        feb2012IdeaHoursCnt += getWeekDayHours(febStart, validTo, validFrom, validTo);
+                    }else if(json[i].ScheduleState == "Defined"){
+                        feb2012DefinedHoursCnt += getWeekDayHours(febStart, validTo, validFrom, validTo);
+                    }
+
                 }
             }
         }
 
-        
+
+        console.log('Total hours spent in each scheduled state across the pieces of work in feb 2012\n\n');
+        console.log('Idea : ' + feb2012IdeaHoursCnt);
+        console.log('Accepted : ' + feb2012AcceptedHoursCnt);
+        console.log('Completed : ' + feb2012CompletedHoursCnt);
+        console.log('Defined : ' + feb2012DefinedHoursCnt);
+        console.log('In-Progress : ' + feb2012InProgressHoursCnt);
 
     });
 
@@ -50,17 +114,31 @@ http.get("http://maccherone.com/share/1000-snapshots-overlap-with-Feb-2012.json"
    console.log("Got error: " + e.message);
 });
 
-function getWeekDays(startDate, endDate){
-    var weekDays = 0;
+
+function getWeekDayHours(startDate, endDate, validFrom, validTo){
+    var weekDayHours = 0;
     var currentDay = startDate;
+    var currentDay09AM;
+    var currentDay05PM;
 
     while(currentDay.isBefore(endDate) || currentDay.isSame(endDate)){
+
+        currentDay09AM = moment(currentDay.format("YYYY-MM-DD") + "T09:30:52.851Z");
+        currentDay05PM = moment(currentDay.format("YYYY-MM-DD") + "T17:30:52.851Z");
+
         if(currentDay.day() != 0 && currentDay.day() != 6){
-            weekDays++;
+
+            if(validFrom.isBefore(currentDay09AM) && validTo.isAfter(currentDay05PM)){
+                weekDayHours += 8;
+            }else if(validFrom.isBefore(currentDay09AM)){
+                weekDayHours += validTo.getHours() - currentDay09AM.getHours();
+            }else if(validTo.isAfter(currentDay05PM)){
+                weekDayHours += currentDay05PM.getHours() - validFrom.getHours();
+            }
         }
         currentDay.add('days',1);
     }
 
-    return weekDays;
+    return weekDayHours;
 }
 
--- momentsample.js
+++ momentsample.js
@@ -1,12 +1,29 @@
 var moment = require('./moment');
 
+/*
 console.log(moment().diff(moment('2009-05-16'), 'days'));
 console.log(moment().isAfter(moment('2009-05-16')));
 console.log(moment('2009-05-16').isBefore(moment()));
+console.log(moment('2009-05-16').isSame(moment()));
 console.log(moment().add('days',2).day());
+console.log(moment().day());
 console.log(moment("2012-01-20T01:30:52.851Z", "YYYY-MM-DDTHH:mm:ss.SSSZ").day());
 console.log(moment('9999-01-01T00:00:00.000Z').isAfter(moment('2012-01-20T01:30:52.851Z')));
 console.log(getWeekDays(moment('2013-01-01T00:00:00.000Z'), moment('2013-01-31T00:00:00.000Z')));
+*/
+
+console.log(moment("2012-01-20T01:30:52.851Z", "YYYY-MM-DDTHH:mm:ss.SSSZ").hours());
+console.log(moment("2012-01-20T01:30:52.851Z", "YYYY-MM-DDTHH:mm:ss.SSSZ").minutes());
+console.log(moment("2012-01-20T01:30:52.851Z", "YYYY-MM-DDTHH:mm:ss.SSSZ").seconds());
+console.log(moment("2012-01-20T01:30:52.851Z", "YYYY-MM-DDTHH:mm:ss.SSSZ").milliseconds());
+var test = moment("2012-01-20T01:30:52.851Z", "YYYY-MM-DDTHH:mm:ss.SSSZ");
+console.log(moment(test.format("YYYY-MM-DD") + "T09:30:52.851Z").format());
+console.log(moment(test.format("YYYY-MM-DD") + "T17:52.851Z").format());
+console.log(moment().year() == '2013');
+
+console.log(moment('2012-02-15T00:25:10.653Z').isAfter(moment('2012-02-01T00:00:00.000Z')));
+
+
 
 
 function getWeekDays(startDate, endDate){
